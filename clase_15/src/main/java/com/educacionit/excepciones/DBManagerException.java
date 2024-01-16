06ca822df7431394d64a220791dace4d4af101e5
Alejandro Di Stefano <addistefano@live.com.ar>
2024-01-15 22:22:54 -0300

    Mejora en el Manejo de Excepciones

--- clase_15/src/main/java/com/educacionit/excepciones/DBManagerException.java
+++ clase_15/src/main/java/com/educacionit/excepciones/DBManagerException.java
@@ -3,27 +3,21 @@ package com.educacionit.excepciones;
 public class DBManagerException extends Exception {
 
 	public DBManagerException() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public DBManagerException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public DBManagerException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public DBManagerException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
+		
 	}
-
-	public DBManagerException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
+	
 
 }
