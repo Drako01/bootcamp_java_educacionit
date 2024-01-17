afb779465cd84527e8d59f00093cd3949270ccb4
Alejandro Di Stefano <addistefano@live.com.ar>
2024-01-17 20:52:02 -0300

    Clase 16

---
+++ clase_16/src/main/java/com/educacionit/excepciones/Ejercicio_01.java
@@ -0,0 +1,14 @@
+package com.educacionit.excepciones;
+
+public class Ejercicio_01 {
+
+	public Ejercicio_01() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
