53707d149ebcf1d77d38784efccb80eaddc563c1
Alejandro Di Stefano <addistefano@live.com.ar>
2024-01-17 20:45:52 -0300

    Mejoras en el Manejo de Excepciones + Mejoras en DBManager

--- clase_15/src/main/java/com/educacionit/dbmanager/DBManager.java
+++ clase_15/src/main/java/com/educacionit/dbmanager/DBManager.java
@@ -6,198 +6,286 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import com.educacionit.excepciones.DBManagerException;
 
 public class DBManager {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/bootcamp";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+	private static final String URL = "jdbc:mysql://localhost:3306/bootcamp";
+	private static final String USER = "root";
+	private static final String PASSWORD = "";
 
-    private Connection conn;
+	private Connection conn;	
+		
+	public void conectar() throws DBManagerException {
+		try {
+			conn = DriverManager.getConnection(URL, USER, PASSWORD);
+			System.out.println("Conexión exitosa a la base de datos");
+		} catch (SQLException e) {
 
-    public void conectar() throws DBManagerException{
-        try {
-            conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Conexión exitosa a la base de datos");
-        } catch (SQLException e) {
-        	System.err.println("Error al conectar a la base de datos: " + e.getMessage());
-        	
-        	throw new DBManagerException("Error al conectar a la base de datos" + e);
-        	
-        } finally {
-        	System.out.println("Fin Bloque de Conexión a la base de datos");
-        }
-    }
+			throw new DBManagerException("Error al conectar a la base de datos" + e.getMessage(), e);
 
+		} finally {
+			System.out.println("Fin Bloque de Conexión a la base de datos");
+		}
+	}
 
-    public void verificarYCrearTabla() throws DBManagerException{
-    	
-    	String table = "alumnos";
-    	Statement statement = null;
-    	
-        try {
-            statement = conn.createStatement();
-            String query = "CREATE TABLE IF NOT EXISTS " + table + " (" + "legajo INT PRIMARY KEY,"
-                    + "nombre VARCHAR(255) NOT NULL," + "edad INT NOT NULL," + "especialidad VARCHAR(255) NOT NULL)";
-            statement.executeUpdate(query);
-            
-            System.out.println("Tabla 'alumnos' verificada y creada si no existía");       
-                       
-        } catch (SQLException e) {
-            System.err.println("Error al verificar y crear la tabla: " + e.getMessage());
-            throw new DBManagerException("Error al verificar y crear la tabla: " + e);
-            
-        } finally {
-        	try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                System.err.println("Error al cerrar el statement: " + e.getMessage());
-            }
-        }
-    }
+	public void verificarYCrearTabla() throws DBManagerException {
 
-    public void mostrarAlumnos() throws DBManagerException{
-        String query = "SELECT legajo, nombre, edad, especialidad FROM alumnos";
-        Statement statement = null;
-        ResultSet resultSet = null;
-        try {
-            statement = conn.createStatement();
-            resultSet = statement.executeQuery(query);
+		String table = "alumnos";
+		Statement statement = null;
 
-            while (resultSet.next()) {
-                int legajo = resultSet.getInt("legajo");
-                String nombre = resultSet.getString("nombre");
-                int edad = resultSet.getInt("edad");
-                String especialidad = resultSet.getString("especialidad");
+		try {
+			statement = conn.createStatement();
+			String query = "CREATE TABLE IF NOT EXISTS " + table + " (" + "legajo INT PRIMARY KEY,"
+					+ "nombre VARCHAR(255) NOT NULL," + "edad INT NOT NULL," + "especialidad VARCHAR(255) NOT NULL)";
+			statement.executeUpdate(query);
 
-                System.out.println("Legajo: " + legajo + ", Nombre: " + nombre + ", Edad: " + edad + ", Especialidad: "
-                        + especialidad);
-            }
-        } catch (SQLException e) {
-            System.err.println("Error al mostrar alumnos: " + e.getMessage());
-            throw new DBManagerException("Error al mostrar alumnos: " + e);
-        } finally {
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-            } catch (SQLException e) {
-                System.err.println("Error al cerrar el resultSet: " + e.getMessage());                
-            }
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                System.err.println("Error al cerrar el statement: " + e.getMessage());
-            }
-        }
-    }
+			System.out.println("Tabla 'alumnos' verificada y creada si no existía");
 
-    public void insertarAlumno(Alumno alumno) throws DBManagerException{
-        String query = "INSERT INTO alumnos (legajo, nombre, edad, especialidad) VALUES (?, ?, ?, ?)";
-        PreparedStatement statement = null;
-        try {
-            statement = conn.prepareStatement(query);
-            statement.setInt(1, alumno.getLegajo());
-            statement.setString(2, alumno.getNombre());
-            statement.setInt(3, alumno.getEdad());
-            statement.setString(4, alumno.getEspecialidad());
+		} catch (SQLException e) {
 
-            int rowsAffected = statement.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("El Alumno " + alumno + " fue insertado correctamente");
-            } else {
-                System.out.println("No se pudo insertar el alumno: " + alumno);
-                throw new DBManagerException("No se pudo insertar el alumno: " + alumno);
-            }
-        } catch (SQLException e) {
-            System.err.println("Error al insertar alumno: " + e.getMessage());
-            throw new DBManagerException("Error al insertar alumno: " + e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                System.err.println("Error al cerrar el statement: " + e.getMessage());
-            }
-        }
-    }
+			throw new DBManagerException("Error al verificar y crear la tabla " + e.getMessage(), e);
 
-    public void modificarAlumno(Alumno alumno) throws DBManagerException{
-        PreparedStatement statement = null;
-        try {
-            String query = "UPDATE alumnos SET nombre = ?, edad = ?, especialidad = ? WHERE legajo = ?";
-            statement = conn.prepareStatement(query);
-            statement.setString(1, alumno.getNombre());
-            statement.setInt(2, alumno.getEdad());
-            statement.setString(3, alumno.getEspecialidad());
-            statement.setInt(4, alumno.getLegajo());
+		} finally {
+			try {
+				if (statement != null) {
+					statement.close();
+				}
+			} catch (SQLException e) {
 
-            int filasAfectadas = statement.executeUpdate();
-            if (filasAfectadas > 0) {
-                System.out.println("El Alumno " + alumno + " modificado correctamente");
-            } else {
-                System.out.println("No se pudo modificar el alumno: " + alumno);
-                throw new DBManagerException("No se pudo modificar el alumno: " + alumno);
-            }
-        } catch (SQLException e) {
-            System.err.println("Error al modificar alumno: " + e.getMessage());
-            throw new DBManagerException("Error al modificar alumno" + e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                System.err.println("Error al cerrar el statement: " + e.getMessage());
-            }
-        }
-    }
+				throw new DBManagerException("Error al cerrar el statement: " + e.getMessage(), e);
+			}
+		}
+	}
 
-    public void eliminarAlumno(Integer legajo) throws DBManagerException{
-        PreparedStatement statement = null;
-        try {
-            String query = "DELETE FROM alumnos WHERE legajo = ?";
-            statement = conn.prepareStatement(query);
-            statement.setInt(1, legajo);
+	public ArrayList<Alumno> mostrarAlumnos() throws DBManagerException {
+		ArrayList<Alumno> listaAlumnos = new ArrayList<Alumno>();
 
-            int filasAfectadas = statement.executeUpdate();
-            if (filasAfectadas > 0) {
-                System.out.println("El Alumno con Legajo: " + legajo + " ha sido eliminado correctamente");
-            } else {
-                System.out.println("No se pudo eliminar el alumno con Legajo: " + legajo);
-                throw new DBManagerException("No se pudo eliminar el alumno con Legajo: " + legajo);
-            }
-        } catch (SQLException e) {
-            System.err.println("Error al eliminar alumno: " + e.getMessage());
-            throw new DBManagerException("Error al eliminar alumno" + e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                System.err.println("Error al cerrar el statement: " + e.getMessage());
-            }
-        }
-    }
+		String query = "SELECT legajo, nombre, edad, especialidad FROM alumnos";
+		Statement statement = null;
+		ResultSet resultSet = null;
 
+		try {
+			statement = conn.createStatement();
+			resultSet = statement.executeQuery(query);
 
-    public void closeConnection() throws DBManagerException{
-        try {
-            if (conn != null) {
-                conn.close();
-                System.out.println("Conexión cerrada");
-            }
-        } catch (SQLException e) {
-            System.err.println("Error al cerrar la conexión: " + e.getMessage());
-            throw new DBManagerException("Error al cerrar la conexión: " + e);
-        }
-    }
+			while (resultSet.next()) {
+				int legajo = resultSet.getInt(1);
+				String nombre = resultSet.getString(2);
+				int edad = resultSet.getInt(3);
+				String especialidad = resultSet.getString(4);
+
+				Alumno unAlumno = new Alumno(legajo, nombre);
+				unAlumno.setEdad(edad);
+				unAlumno.setEspecialidad(especialidad);
+
+				listaAlumnos.add(unAlumno);
+
+			}
+
+		} catch (SQLException e) {
+
+			throw new DBManagerException("Error al mostrar alumnos: " + e.getMessage(), e);
+
+		} finally {
+			try {
+				if (resultSet != null) {
+					resultSet.close();
+				}
+
+			} catch (SQLException e) {
+
+				System.err.println("Error al cerrar el resultSet" + e.getMessage());
+			}
+			try {
+				if (statement != null) {
+					statement.close();
+				}
+
+			} catch (SQLException e) {
+
+				System.err.println("Error al cerrar el statement" + e.getMessage());
+			}
+		}
+
+		return listaAlumnos;
+
+	}
+
+	public Alumno obtenerUnAlumno(Integer legajo) throws DBManagerException {
+
+		String query = "SELECT nombre, edad, especialidad FROM alumnos WHERE legajo = ?";
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			statement = conn.prepareStatement(query);
+			statement.setInt(1, legajo);
+
+			resultSet = statement.executeQuery();
+
+			Boolean hayRegistros = resultSet.next();
+
+			if (hayRegistros == false) {
+				throw new DBManagerException("Error al mostrar el alumno de legajo: " + legajo);
+			}
+
+			String nombre = resultSet.getString("nombre");
+			Alumno unAlumno = new Alumno(legajo, nombre);
+			Integer edad = resultSet.getInt(2);
+			String especialidad = resultSet.getString(3);
+
+			unAlumno.setEdad(edad);
+			unAlumno.setEspecialidad(especialidad);
+
+			return unAlumno;
+
+		} catch (SQLException e) {
+
+			throw new DBManagerException(
+					"Error al mostrar el alumno de legajo: " + legajo + " por el Error: " + e.getMessage(), e);
+
+		} finally {
+			try {
+				if (resultSet != null) {
+					resultSet.close();
+				}
+
+			} catch (SQLException e) {
+
+				System.err.println("Error al cerrar el resultSet" + e.getMessage());
+			}
+			try {
+				if (statement != null) {
+					statement.close();
+				}
+
+			} catch (SQLException e) {
+
+				System.err.println("Error al cerrar el statement" + e.getMessage());
+			}
+		}
+
+	}
+
+	public void insertarAlumno(Alumno alumno) throws DBManagerException {
+
+		String query = "INSERT INTO alumnos (legajo, nombre, edad, especialidad) VALUES (?, ?, ?, ?)";
+		PreparedStatement statement = null;
+		try {
+			statement = conn.prepareStatement(query);
+			statement.setInt(1, alumno.getLegajo());
+			statement.setString(2, alumno.getNombre());
+			statement.setInt(3, alumno.getEdad());
+			statement.setString(4, alumno.getEspecialidad());
+
+			int rowsAffected = statement.executeUpdate();
+
+			if (rowsAffected == 0) {
+				throw new DBManagerException("No se pudo insertar el alumno: " + alumno);
+			}
+			System.out.println("El Alumno " + alumno + " modificado correctamente");
+
+		} catch (SQLException e) {
+
+			throw new DBManagerException("Error al insertar alumno: " + e.getMessage(), e);
+
+		} finally {
+
+			try {
+				if (statement != null) {
+					statement.close();
+				}
+
+			} catch (SQLException e) {
+
+				System.err.println("Error al cerrar el statement" + e.getMessage());
+			}
+		}
+	}
+
+	public void modificarAlumno(Alumno alumno) throws DBManagerException {
+
+		PreparedStatement statement = null;
+
+		try {
+			String query = "UPDATE alumnos SET nombre = ?, edad = ?, especialidad = ? WHERE legajo = ?";
+			statement = conn.prepareStatement(query);
+			statement.setString(1, alumno.getNombre());
+			statement.setInt(2, alumno.getEdad());
+			statement.setString(3, alumno.getEspecialidad());
+			statement.setInt(4, alumno.getLegajo());
+
+			int rowsAffected = statement.executeUpdate();
+			if (rowsAffected == 0) {
+				throw new DBManagerException("No se pudo insertar el alumno: " + alumno);
+			}
+			System.out.println("El Alumno " + alumno + " modificado correctamente");
+		} catch (SQLException e) {
+
+			throw new DBManagerException("Error al modificar alumno" + e.getMessage(), e);
+
+		} finally {
+
+			try {
+				if (statement != null) {
+					statement.close();
+				}
+
+			} catch (SQLException e) {
+
+				System.err.println("Error al cerrar el statement" + e.getMessage());
+			}
+		}
+	}
+
+	public void eliminarAlumno(Integer legajo) throws DBManagerException {
+
+		PreparedStatement statement = null;
+
+		try {
+			String query = "DELETE FROM alumnos WHERE legajo = ?";
+			statement = conn.prepareStatement(query);
+			statement.setInt(1, legajo);
+
+			int rowsAffected = statement.executeUpdate();
+			if (rowsAffected == 0) {
+				System.out.println("No se pudo eliminar el alumno con Legajo: " + legajo);
+			}
+			System.out.println("El Alumno con Legajo: " + legajo + " ha sido eliminado correctamente");
+
+		} catch (SQLException e) {
+
+			throw new DBManagerException("Error al eliminar alumno" + e.getMessage(), e);
+
+		} finally {
+
+			try {
+				if (statement != null) {
+					statement.close();
+				}
+
+			} catch (SQLException e) {
+
+				System.err.println("Error al cerrar el statement" + e.getMessage());
+			}
+		}
+	}
+
+	public void closeConnection() throws DBManagerException {
+		try {
+			if (conn != null) {
+				conn.close();
+				System.out.println("Conexión cerrada");
+			}
+		} catch (SQLException e) {
+
+			throw new DBManagerException("Error al cerrar la conexión: " + e.getMessage(), e);
+
+		} finally {
+
+			System.out.println("Bloque de Cerrar conexión Finalizado");
+		}
+	}
 }
