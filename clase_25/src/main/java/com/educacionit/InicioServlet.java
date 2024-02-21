3c699cef015ecc73bf844f00cd4e0f31e0d4ba0d
Alejandro Di Stefano <addistefano@live.com.ar>
2024-02-21 20:58:58 -0300

    Clase 25 Web App

---
+++ clase_25/src/main/java/com/educacionit/InicioServlet.java
@@ -0,0 +1,50 @@
+package com.educacionit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InicioServlet
+ */
+public class InicioServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public InicioServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		PrintWriter printWriter = response.getWriter();
+		printWriter.append("<html>" + "<body>" + "<h2>Bienvenidos</h2>" + 
+		"<img src='https://avatars.githubusercontent.com/u/88512335?v=4' alt='Drako' />"
+		+ "</body>" + "</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ clase_25/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<jsp:forward page="InicioServlet"></jsp:forward>
+</body>
+</html>
