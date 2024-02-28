731de6ecc24ca77acabedf8045723b3ef52592c5
Alejandro Di Stefano <addistefano@live.com.ar>
2024-02-28 18:42:13 -0300

    Ejercicio 01

---
+++ Desafios/src/main/java/com/educacionit/desafio_07/desafiojava1/CargaDeDatos.java
@@ -0,0 +1,73 @@
+package com.educacionit.desafio_07.desafiojava1;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class CargaDeDatos {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Ingrese la cantidad de alumnos a procesar: ");
+		int cantidadAlumnos = scanner.nextInt();
+		scanner.nextLine();
+
+		Alumno[] alumnos = new Alumno[cantidadAlumnos];
+
+		for (int i = 0; i < cantidadAlumnos; i++) {
+			System.out.println("Ingrese los datos para el alumno " + (i + 1) + ":");
+			System.out.print("Nombre: ");
+			String nombre = scanner.nextLine();
+			System.out.print("Apellido: ");
+			String apellido = scanner.nextLine();
+			System.out.print("Cantidad de exámenes: ");
+			int cantidadExamenes = scanner.nextInt();
+			scanner.nextLine();
+
+			Alumno alumno = new Alumno(nombre, apellido, cantidadExamenes);
+
+			for (int j = 1; j <= cantidadExamenes; j++) {
+			    System.out.print("Nota del Exámen " + j + ": ");
+			    double nota = scanner.nextDouble();
+			    scanner.nextLine();
+			    alumno.getNotas()[j - 1] = nota;
+			}
+
+			alumnos[i] = alumno;
+		}
+
+		List<Alumno> mejoresAlumnos = Resultados.encontrarCalificacionMasAlta(alumnos);
+		System.out.println("Alumno/s con la calificación más alta:");
+		for (Alumno alumno : mejoresAlumnos) {
+			System.out.println(alumno);
+		}
+
+		List<Alumno> peoresAlumnos = Resultados.encontrarCalificacionMasBaja(alumnos);
+		System.out.println("Alumno/s con la calificación más baja:");
+		for (Alumno alumno : peoresAlumnos) {
+			System.out.println(alumno);
+		}
+
+		List<Alumno> promocionAlumnos = Resultados.encontrarAlumnosPromocionan(alumnos);
+		System.out.println("Alumnos que promocionan:");
+		if (promocionAlumnos.isEmpty()) {
+			System.out.println("No hay datos para mostrar");
+		} else {
+			for (Alumno alumno : promocionAlumnos) {
+				System.out.println(alumno);
+			}
+		}
+
+		List<Alumno> recursarAlumnos = Resultados.encontrarAlumnosRecursar(alumnos);
+		System.out.println("Alumnos que deben recursar la materia:");
+		if (recursarAlumnos.isEmpty()) {
+			System.out.println("No hay datos para mostrar");
+		} else {
+			for (Alumno alumno : recursarAlumnos) {
+				System.out.println(alumno);
+			}
+		}
+
+		scanner.close();
+	}
+}
---
+++ Desafios/src/main/java/com/educacionit/desafio_07/desafiojava1/Resultados.java
@@ -0,0 +1,71 @@
+package com.educacionit.desafio_07.desafiojava1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Resultados {
+
+	public static List<Alumno> encontrarCalificacionMasAlta(Alumno[] alumnos) {
+		List<Alumno> mejoresAlumnos = new ArrayList<>();
+		double maxPromedio = 0; 
+
+		for (Alumno alumno : alumnos) {
+			double promedio = alumno.calcularPromedio();
+			if (promedio > maxPromedio) {
+				maxPromedio = promedio;
+			}
+		}
+
+		for (Alumno alumno : alumnos) {
+			if (alumno.calcularPromedio() == maxPromedio) {
+				mejoresAlumnos.add(alumno);
+			}
+		}
+
+		return mejoresAlumnos;
+	}
+
+	public static List<Alumno> encontrarCalificacionMasBaja(Alumno[] alumnos) {
+		List<Alumno> peoresAlumnos = new ArrayList<>();
+		double minPromedio = 10; 
+
+		for (Alumno alumno : alumnos) {
+			double promedio = alumno.calcularPromedio();
+			if (promedio < minPromedio) {
+				minPromedio = promedio;
+			}
+		}
+
+		for (Alumno alumno : alumnos) {
+			if (alumno.calcularPromedio() == minPromedio) {
+				peoresAlumnos.add(alumno);
+			}
+		}
+
+		return peoresAlumnos;
+	}
+
+	public static List<Alumno> encontrarAlumnosPromocionan(Alumno[] alumnos) {
+		List<Alumno> alumnosPromocion = new ArrayList<>();
+
+		for (Alumno alumno : alumnos) {
+			if (alumno.calcularPromedio() >= 7) {
+				alumnosPromocion.add(alumno);
+			}
+		}
+
+		return alumnosPromocion;
+	}
+
+	public static List<Alumno> encontrarAlumnosRecursar(Alumno[] alumnos) {
+		List<Alumno> alumnosRecursar = new ArrayList<>();
+
+		for (Alumno alumno : alumnos) {
+			if (alumno.calcularPromedio() < 7) {
+				alumnosRecursar.add(alumno);
+			}
+		}
+
+		return alumnosRecursar;
+	}
+}
