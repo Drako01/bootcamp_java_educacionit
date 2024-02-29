0d57fe53fad4b6e9eeb88988f61873d34ca37da4
Alejandro Di Stefano <addistefano@live.com.ar>
2024-02-29 15:52:09 -0300

    Clase 28

---
+++ clase_28/src/main/java/com/educacioit/lambda/test/TestLambdas.java
@@ -0,0 +1,31 @@
+package com.educacioit.lambda.test;
+
+import com.educacioit.lambda.Lambdas;
+
+public class TestLambdas {
+
+	public static void main(String[] args) {
+		Lambdas lambda1 = new Lambdas();
+		lambda1.ejercicio01();
+		lambda1.ejercicio02();
+		lambda1.ejercicio03();
+		lambda1.ejercicio04();
+		lambda1.ejercicio05();
+		lambda1.ejercicio06();
+		lambda1.ejercicio07();
+		lambda1.ejercicio08();
+		lambda1.ejercicio09();
+		lambda1.ejercicio10();
+		lambda1.ejercicio11();
+		lambda1.ejercicio12();
+		lambda1.ejercicio13();
+		lambda1.ejercicio14();
+		lambda1.ejercicio15();
+		lambda1.ejercicio16();
+		lambda1.ejercicio17();
+		lambda1.ejercicio18();
+		
+		
+	}
+
+}
