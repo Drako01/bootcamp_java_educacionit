d24656b0b879cfb9a5732ef49fa15743fb8a8064
Alejandro Di Stefano <addistefano@live.com.ar>
2024-03-01 15:55:43 -0300

    Ejercicio 03

--- Desafios/src/main/java/com/educacionit/desafio_07/desafiojava3/Main.java
+++ Desafios/src/main/java/com/educacionit/desafio_07/desafiojava3/Main.java
@@ -1,7 +1,43 @@
 package com.educacionit.desafio_07.desafiojava3;
 
+import com.educacionit.desafio_07.desafiojava3.clase.CollectionCustom;
+
 public class Main {
 	public static void main(String[] args) {
+		CollectionCustom<Integer> coleccion = new CollectionCustom<>();
 
+		System.out.println("isEmpty: " + coleccion.empty());
+
+		coleccion.addFirst(1);
+		coleccion.add(2);
+		coleccion.add(3);
+		coleccion.add(4);
+		coleccion.remove(1);
+		coleccion.addLast(3);
+
+		System.out.println("Coleccion: " + coleccion);
+		
+		coleccion.removeAllWhithDate(3);
+		System.out.println("Coleccion: " + coleccion);
+		System.out.println("isEmpty: " + coleccion.empty());
+
+		CollectionCustom<String> coleccionString = new CollectionCustom<>();
+
+		System.out.println("isEmpty: " + coleccionString.empty());
+
+		coleccionString.addFirst("Hola");
+		coleccionString.add("como");
+		coleccionString.add("estan");
+		coleccionString.add("todos");
+		
+		System.out.println("Coleccion: " + coleccionString);
+		coleccionString.remove("todos");
+		coleccionString.addLast("ustedes");
+
+		System.out.println("Coleccion: " + coleccionString);
+		System.out.println("isEmpty: " + coleccionString.empty());
+		coleccionString.removeAll();
+		System.out.println("Coleccion: " + coleccionString);
+		System.out.println("isEmpty: " + coleccionString.empty());
 	}
 }
